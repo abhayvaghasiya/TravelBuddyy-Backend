13b433f250d16cd34f0c5319c9f936bd66327307
Abhay Kishorbhai Vaghasiya <abhayv@mail.uni-paderborn.de>
2025-06-02 08:01:10 +0200

    Add TravelBuddyApplication class for Spring Boot application setup

---
+++ src/main/java/com/travelbuddy/api/TravelBuddyApplication.java
@@ -0,0 +1,12 @@
+package com.travelbuddy.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelBuddyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelBuddyApplication.class, args);
+    }
+} 
